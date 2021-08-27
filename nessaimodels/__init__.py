92154b22b261eab86c7a5702c7c2789c4a24ffe1
mj-will <m.williams.4@research.gla.ac.uk>
2021-08-27 16:07:59 +0100

    Add GMM with data

--- nessaimodels/__init__.py
+++ nessaimodels/__init__.py
@@ -1,9 +1,10 @@
 from .gaussian import Gaussian
-from .gaussianmixture import GaussianMixture
+from .gaussianmixture import GaussianMixture, GaussianMixtureWithData
 from .rosenbrock import Rosenbrock
 
 __all__ = [
     'Gaussian',
     'GaussianMixture',
-    'Rosenbrock'
+    'GaussianMixtureWithData',
+    'Rosenbrock',
 ]
