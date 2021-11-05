ef01acb1444688a2124d31fc95be7dee52f320ed
Michael J. Williams <michaeljw1@googlemail.com>
2021-11-05 17:13:32 +0000

    Add version and documentation

--- nessai_models/__init__.py
+++ nessai_models/__init__.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Models for use with the nested sampler \
+    `nessai <https://github.com/mj-will/nessai-models>`_.
+"""
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:   # for Python < 3.8
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    pass
+
 from .gaussian import Gaussian
 from .gaussianmixture import GaussianMixture, GaussianMixtureWithData
 from .halfgaussian import HalfGaussian
