a2634619fb057dd9621a46fa9bd1f4dc064b9be0
Michael J. Williams <michaeljw1@googlemail.com>
2021-11-05 16:38:54 +0000

    Add `HalfGaussian` to tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,6 +6,7 @@ from nessaimodels import (
     Gaussian,
     GaussianMixture,
     GaussianMixtureWithData,
+    HalfGaussian,
     Rosenbrock,
 )
 
@@ -13,6 +14,7 @@ models = [
     Gaussian,
     GaussianMixture,
     GaussianMixtureWithData,
+    HalfGaussian,
     Rosenbrock,
 ]
 
