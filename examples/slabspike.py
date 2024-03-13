4d55fda76d8c48d62eeea581f0ec302128002f75
John Veitch <john.veitch@ligo.org>
2024-03-13 09:36:19 -0500

    Run black

--- examples/slabspike.py
+++ examples/slabspike.py
@@ -7,17 +7,21 @@ from nessai.flowsampler import FlowSampler
 from nessai.utils import setup_logger
 from nessai_models import SlabSpike
 import numpy as np
+
 # Set up the logger as normal for nessai
 setup_logger()
 
-model = SlabSpike(weights=[0.01,0.99])
+model = SlabSpike(weights=[0.01, 0.99])
 
 # We then create an instance of the sampler as usual
-fs = FlowSampler(model, output="slab_spike", resume=False,
-                 importance_nested_sampler=True)
-analytic_evidence = -np.log(np.product(list(map(lambda x:(x[1]-x[0]),model.bounds.values()))))
+fs = FlowSampler(
+    model, output="slab_spike", resume=False, importance_nested_sampler=True
+)
+analytic_evidence = -np.log(
+    np.product(list(map(lambda x: (x[1] - x[0]), model.bounds.values())))
+)
 
 # And run the sampler
 fs.run()
-print(f'analytic logZ = {analytic_evidence}, estimated logZ={fs.logZ}')
-print(f'Difference = {analytic_evidence - fs.logZ}')
+print(f"analytic logZ = {analytic_evidence}, estimated logZ={fs.logZ}")
+print(f"Difference = {analytic_evidence - fs.logZ}")
